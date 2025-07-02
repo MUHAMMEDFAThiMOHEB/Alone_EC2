974f3a575ebac6419cabbae31e6bb4b4f60af1dd
Muhammad Fathy Moheb <mohamed192509@fci.bu.edu.eg>
2025-07-02 18:56:35 +0300

    Upload all file.tf + the related assets

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Alone_EC2
-My First Terraform Project, covering the some core concepts and services in AWS
+My First Terraform Project, covering the some core concepts and services in AWS (VPC, EC2, Subnets, Security groups) + output block
 
 All the requirments and the writen solution in requirments.txt file
 
 this project is the the first Actual project in a Terraform course i study with
+
+Here is the diagram of the solution
+
+Note: you will find in the Assest directory 2 Photos of the diagram one is a screenshot for show only
+      the other one is .drawio.png, this one is editable if you want to customize the diagram
+
+![Solution Diagram](./Assets/Diagram%20S%20O.png)
+
+1- initiation process
+
+![terraform init](./Assets/init.png)
+
+2- Validation and plan commands run
+
+![terraform plan](./Assets/validate%20&%20plan.png)
+
+3- Apply the Infrustructure on AWS
+
+![terraform apply](./Assets/apply.png)
+
+4- Terraform destory command
+
+![terraform destroy](./Assets/destroy.png)
---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = "T_User_Temp"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "allow_tls" {
+    name        = "allow_tls"
+    description = "Allow TLS inbound traffic and all outbound traffic"
+    vpc_id      = aws_vpc.main.id
+
+    tags = {
+        Name = "allow_tls"
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+    security_group_id = aws_security_group.allow_tls.id
+    cidr_ipv4         = aws_vpc.main.cidr_block
+    from_port         = 443
+    ip_protocol       = "tcp"
+    to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+    security_group_id = aws_security_group.allow_tls.id
+    cidr_ipv4         = "0.0.0.0/0"
+    ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_subnet" "Public_Subnet" {
+    vpc_id = aws_vpc.main.id
+    availability_zone = "us-east-1a"
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "Public_Subnet"
+    }
+}
+
+resource "aws_instance" "EC2-Instance" {
+    ami = "ami-05ffe3c48a9991133"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.Public_Subnet.id
+    associate_public_ip_address = "true"
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    tags = {
+        Name = "Alone-Instance"
+    }
+}
+
+output "ec2_public" {
+    value = aws_instance.EC2-Instance.public_ip
+    depends_on = [ aws_vpc_security_group_ingress_rule.allow_tls_ipv4 ]
+}
